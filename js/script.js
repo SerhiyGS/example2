59d799e0277226326d04f2e7639acdbd9095c23c
Serhiy <123894736+SerhiyGS@users.noreply.github.com>
2023-02-05 01:08:37 +0200

    01.08.05.02.23

---
+++ js/script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const SendMSG = (msg, request, answer, imgfile)=>{
+    let formData = new FormData();
+     formData.append("rezhim",msg);
+     formData.append("zapyt",request);
+     if(imgfile) formData.append("imgfile",imgfile);
+ 
+    var x = new XMLHttpRequest();
+    x.open("POST", "http://serhiy.my-board.org/001/index.php", true);
+    x.send(formData);
+    x.onload = ()=>{
+         //let nove = Math.floor(Date.now() / 1000) - Zaraz;
+         let ar;
+       let radochok = '';
+         switch (msg) {
+          case 'ur0'://Оновлення поточного часу у записі клієнта і обнулення фантомів
+ /*
+             ar = JSON.parse(x.responseText);
+             //console.log(x.responseText);
+             //console.log(ar[0].answer);
+             if(ar[0].answer == 'exit') {
+                //console.log(ar[0].answer);
+                window.location.href='./index.php';
+             }
+ */
+             break;
+
+
+
+             default:
+                alert ('answer.innerHTML = x.responseText');
+              break;
+           }
+        }
+     }        
+
+//let JSstr = JSON.stringify([G_Param1, G_Param2, G_Param3, par.last_time]);
+let G_Param1, G_Param2, G_Param3, last_time;
+G_Param1='login';
+G_Param2='password';
+G_Param3='sid';
+G_Param4=new Date().getTime();
+
+
+let JSstr = JSON.stringify([G_Param1, G_Param2, G_Param3, G_Param4]);
+SendMSG('ur0', JSstr, '', '' );
+  
